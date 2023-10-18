32c6eb8f4bb378619b7516f6a49a003e605bf518
ganavigr2002 <142506800+ganavigr2002@users.noreply.github.com>
2023-10-18 12:34:07 +0530

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,111 +1,62 @@
-import PropTypes from 'prop-types';
-import NextLink from 'next/link';
-import { Box, Typography, Unstable_Grid2 as Grid } from '@mui/material';
-import { Logo } from 'src/components/logo';
+import { useCallback, useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { styled } from '@mui/material/styles';
+import { withAuthGuard } from 'src/hocs/with-auth-guard';
+import { SideNav } from './side-nav';
+import { TopNav } from './top-nav';
 
-// TODO: Change subtitle text
+const SIDE_NAV_WIDTH = 280;
 
-export const Layout = (props) => {
+const LayoutRoot = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flex: '1 1 auto',
+  maxWidth: '100%',
+  [theme.breakpoints.up('lg')]: {
+    paddingLeft: SIDE_NAV_WIDTH
+  }
+}));
+
+const LayoutContainer = styled('div')({
+  display: 'flex',
+  flex: '1 1 auto',
+  flexDirection: 'column',
+  width: '100%'
+});
+
+export const Layout = withAuthGuard((props) => {
   const { children } = props;
+  const pathname = usePathname();
+  const [openNav, setOpenNav] = useState(false);
+
+  const handlePathnameChange = useCallback(
+    () => {
+      if (openNav) {
+        setOpenNav(false);
+      }
+    },
+    [openNav]
+  );
+
+  useEffect(
+    () => {
+      handlePathnameChange();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pathname]
+  );
 
   return (
-    <Box
-      component="main"
-      sx={{
-        display: 'flex',
-        flex: '1 1 auto'
-      }}
-    >
-      <Grid
-        container
-        sx={{ flex: '1 1 auto' }}
-      >
-        <Grid
-          xs={12}
-          lg={6}
-          sx={{
-            backgroundColor: 'background.paper',
-            display: 'flex',
-            flexDirection: 'column',
-            position: 'relative'
-          }}
-        >
-          <Box
-            component="header"
-            sx={{
-              left: 0,
-              p: 3,
-              position: 'fixed',
-              top: 0,
-              width: '100%'
-            }}
-          >
-            <Box
-              component={NextLink}
-              href="/"
-              sx={{
-                display: 'inline-flex',
-                height: 32,
-                width: 32
-              }}
-            >
-              <Logo />
-            </Box>
-          </Box>
+    <>
+      <TopNav onNavOpen={() => setOpenNav(true)} />
+      <SideNav
+        onClose={() => setOpenNav(false)}
+        open={openNav}
+      />
+      <LayoutRoot>
+        <LayoutContainer>
           {children}
-        </Grid>
-        <Grid
-          xs={12}
-          lg={6}
-          sx={{
-            alignItems: 'center',
-            background: 'radial-gradient(50% 50% at 50% 50%, #122647 0%, #090E23 100%)',
-            color: 'white',
-            display: 'flex',
-            justifyContent: 'center',
-            '& img': {
-              maxWidth: '100%'
-            }
-          }}
-        >
-          <Box sx={{ p: 3 }}>
-            <Typography
-              align="center"
-              color="inherit"
-              sx={{
-                fontSize: '24px',
-                lineHeight: '32px',
-                mb: 1
-              }}
-              variant="h1"
-            >
-              Welcome to{' '}
-              <Box
-                component="a"
-                sx={{ color: '#15B79E' }}
-                target="_blank"
-              >
-                Devias Kit
-              </Box>
-            </Typography>
-            <Typography
-              align="center"
-              sx={{ mb: 3 }}
-              variant="subtitle1"
-            >
-              A professional kit that comes with ready-to-use MUI components.
-            </Typography>
-            <img
-              alt=""
-              src="/assets/auth-illustration.svg"
-            />
-          </Box>
-        </Grid>
-      </Grid>
-    </Box>
+        </LayoutContainer>
+      </LayoutRoot>
+    </>
   );
-};
-
-Layout.prototypes = {
-  children: PropTypes.node
-};
+});
